f08aab9de59f730d5d406650f7f03fbad0eb7b9f
thorne7 <darienhallam@gmail.com>
2024-08-27 15:44:12 +0100

    Initial Commit

---
+++ Fantasy Name Generator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fantasy Name Generator
+//
+//  Created by Darien Hallam on 27/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fantasy Name Generator/Fantasy_Name_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fantasy_Name_GeneratorApp.swift
+//  Fantasy Name Generator
+//
+//  Created by Darien Hallam on 27/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct Fantasy_Name_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
